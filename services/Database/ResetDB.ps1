970aa46c2c679642d978de761de9adc2865a157f
immybo <immybo@users.noreply.github.com>
2020-02-28 20:29:43 +1300

    Set up CI with Azure Pipelines
    [skip ci]

--- services/Database/ResetDB.ps1
+++ services/Database/ResetDB.ps1
@@ -1,4 +1,4 @@
-$server = "(LocalDb)\MSSQLLocalDB"
+$server = $args[0]
 $scriptDir = ".\"
 $logFile = "$scriptDir\ResetLog.txt"
 
@@ -6,22 +6,25 @@ $teardownScripts = Get-ChildItem $scriptDir\Teardown\*.sql
 $databaseSchemaScripts = Get-ChildItem $scriptDir\Schema\Database\*.sql
 $schemaScripts = Get-ChildItem $scriptDir\Schema\*.sql
 
+$dbUsername = $args[1]
+$dbPassword = $args[2]
+
 Clear-Content $logFile
 
 ForEach ($script in $teardownScripts)
 {
 	"Running teardown script: $script" | out-file $logFile -Append
-	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server
+	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server -username $dbUSername -password $dbPassword
 }
 
 ForEach ($script in $databaseSchemaScripts)
 {
 	"Running database schema script: $script" | out-file $logFile -Append
-	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server
+	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server -username $dbUSername -password $dbPassword
 }
 
 ForEach ($script in $schemaScripts)
 {
 	"Running schema script: $script" | out-file $logFile -Append
-	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server
+	Invoke-SqlCmd -inputfile $script.FullName -serverinstance $server -username $dbUSername -password $dbPassword
 }
