e90aa3d825e627e8593645cf11464dd764a0b81d
R. Campbell <robertcampbellnz@gmail.com>
2020-05-28 20:33:29 +1200

    Allow decimal places in nutrition.

--- mobile/src/util/Regex.ts
+++ mobile/src/util/Regex.ts
@@ -1,1 +1,2 @@
 export const Numbers = /^\d+$/
+export const NumbersWithDecimalPlace = /^\d+\.?\d*$/ // Note that this allows no number after the decimal place, since JS can still parse this.
--- mobile/src/util/ValidationRules.ts
+++ mobile/src/util/ValidationRules.ts
@@ -1,4 +1,4 @@
-import { Numbers } from "./Regex";
+import { NumbersWithDecimalPlace } from "./Regex";
 import { ValidationRule } from "../components/shared/Validator";
 
-export const PositiveOrZero: ValidationRule<string> = { rule: value => Numbers.test(value) || value == "", errorMessage: _ => "Please enter a positive number or 0." };
+export const PositiveOrZero: ValidationRule<string> = { rule: value => NumbersWithDecimalPlace.test(value) || value == "", errorMessage: _ => "Please enter a positive number or 0." };
